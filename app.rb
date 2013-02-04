12b367f251abef88d861676c52bfe54416d66bdd
Daniel Hertz <daniel@dhertz.com>
2013-02-04 17:48:57 +0000

    validate config

--- app.rb
+++ app.rb
@@ -98,6 +98,7 @@ post "/validate_config/" do
     status 200
     { :valid => true}.to_json
   else 
+    status 401
     { :valid => false, :errors => "Incorrect tweetage amount."}.to_json
   end
 end
